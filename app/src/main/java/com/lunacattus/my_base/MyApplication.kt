b362a253a9da6433f0777e748b84c3ab5039658b
zhoufb <zhoufb@nanjingdadao.com>
2024-05-29 11:06:45 +0800

    change java file to kotlin

--- app/src/main/java/com/lunacattus/my_base/MyApplication.kt
+++ app/src/main/java/com/lunacattus/my_base/MyApplication.kt
@@ -5,6 +5,4 @@ import android.app.Application;
 import dagger.hilt.android.HiltAndroidApp;
 
 @HiltAndroidApp
-public class MyApplication extends Application {
-
-}
+class MyApplication : Application()
